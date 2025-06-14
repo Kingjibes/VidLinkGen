49f4c73f324dfbdedba7b81a89466b6bd507e2e2
Kingjibes <160682090+Kingjibes@users.noreply.github.com>
2025-06-14 23:56:42 +0000

    Update utils.js

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,16 @@
-import { clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+
+import { clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs) {
-	return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
+
+export const generateShortId = (length = 4) => {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+};
