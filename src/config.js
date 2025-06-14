6aacf9f72aa91cd2c137f1a629b581977f234093
Kingjibes <160682090+Kingjibes@users.noreply.github.com>
2025-06-14 23:42:55 +0000

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,16 @@
+const config = {
+  contact: {
+    whatsappNumbers: [
+      { id: 'whatsapp1', number: '+233557488116', channelUrl: 'https://whatsapp.com/channel/0029VaN2eQQ59PwNixDnvD16/7593' },
+      { id: 'CIPHER-MD UPDATE', number: '+233504759307', channelUrl: 'https://whatsapp.com/channel/0029VbAUgm5Fi8xfcJspqi3f' }, 
+    ],
+    telegramHandles: [
+      { id: 'telegram1', username: '', link: 'https://t.me/HACK_ERPRO' },
+      { id: 'telegram2', username: '', link: 'https://t.me/Richvybs6' },
+    ],
+    momoNumber: '0557488116',
+  },
+  premiumPrice: '$2',
+};
+
+export default config;
