cdb3d4b29f9611fda357667f8cd6b0fd7a9e80fb
Kingjibes <160682090+Kingjibes@users.noreply.github.com>
2025-06-20 12:55:17 +0000

    Delete src directory

--- src/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://hdrbkhfnkrjygxftyrel.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhkcmJraGZua3JqeWd4ZnR5cmVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk3Mzc3NjQsImV4cCI6MjA2NTMxMzc2NH0.9T4_Xw2VZgnNtN4q5zN7kqjhl-LR_CIJPrpteb_MrfU';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
